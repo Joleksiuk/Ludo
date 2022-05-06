baaf91526aa9b86e66fffd21164cb819d374ece5
Jan Rokicki <rokicki.jan@gmail.com>
2022-05-06 17:28:27 +0200

    Initial commit

---
+++ src/main/java/pl/rokolujka/springreactludo/SpringReactLudoApplication.java
@@ -0,0 +1,13 @@
+package pl.rokolujka.springreactludo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactLudoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactLudoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rokolujka/springreactludo/SpringReactLudoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rokolujka.springreactludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactLudoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
