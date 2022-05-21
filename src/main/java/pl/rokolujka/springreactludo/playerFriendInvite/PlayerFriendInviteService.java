cb2548389490bcfdeb14ec21ec95bd7679580336
Joleksiuk <julkaoleksiuk26@gmail.com>
2022-05-21 13:26:27 +0200

    Resolved conflicts

--- src/main/java/pl/rokolujka/springreactludo/playerFriendInvite/PlayerFriendInviteService.java
+++ src/main/java/pl/rokolujka/springreactludo/playerFriendInvite/PlayerFriendInviteService.java
@@ -1,7 +1,11 @@
 package pl.rokolujka.springreactludo.playerFriendInvite;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.rokolujka.springreactludo.rabbitMQ.FriendInviteNotification;
+import pl.rokolujka.springreactludo.rabbitMQ.RabbitConfig;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -9,6 +13,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class PlayerFriendInviteService {
+
+    private final RabbitTemplate rabbitTemplate;
 
     private final PlayerFriendInviteRepository playerFriendInviteRepository;
 
@@ -23,6 +29,17 @@ public class PlayerFriendInviteService {
     }
     public void deletePlayerFriendInvite(PlayerFriendInvite playerFriendInvite) {
         playerFriendInviteRepository.delete(playerFriendInvite);
+    }
+
+    public void sendFriendInvite(PlayerFriendInvite playerFriendInvite){
+
+        createPlayerFriendInvite(playerFriendInvite);
+
+        //send friend invite notification to rabbitMQ queue
+        String message = "Player ["+playerFriendInvite.getInvitingUserId()+"] sent friend request to player ["+ playerFriendInvite.getInvitedUserId() +"]";
+        FriendInviteNotification friendInviteNotification = new FriendInviteNotification(playerFriendInvite,message);
+
+        rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE,RabbitConfig.ROUTING_KEY, friendInviteNotification);
     }
 
 }
